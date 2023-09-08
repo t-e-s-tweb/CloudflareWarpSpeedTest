ba2bd26a6b25bee69135763d9e837b7bcd79aa29
t-e-s-tweb <King.Khanrocks.khan@gmail.com>
2023-09-08 23:35:52 +0500

    Update main.go

--- main.go
+++ main.go
@@ -15,15 +15,15 @@ CloudflareWarpSpeedTest \n` + `
 测试 Cloudflare Warp 所有 IP 的延迟和速度，获取最低延迟和端口
 
 参数：
-    -n 200
+    -n 1
         延迟测速线程；越多延迟测速越快，性能弱的设备 (如路由器) 请勿太高；(默认 200 最多 1000)
-    -t 10
+    -t 1
         延迟测速次数；单个 IP 延迟测速的次数；(默认 10 次)
     -q 
         快速模式； 随机扫描5000的地址测速结果
-    -tl 300
+    -tl 1000
         平均延迟上限；只输出低于指定平均延迟的 IP，各上下限条件可搭配使用；(默认 300 ms)
-    -tll 40
+    -tll 0
         平均延迟下限；只输出高于指定平均延迟的 IP；(默认 0 ms)
     -tlr 0.2
         丢包几率上限；只输出低于/等于指定丢包率的 IP，范围 0.00~1.00，0 过滤掉任何丢包的 IP；(默认 1.00)
@@ -36,7 +36,7 @@ CloudflareWarpSpeedTest \n` + `
         IP段数据文件；如路径含有空格请加上引号；支持其他 CDN IP段；(默认 ip.txt)
     -ip 1.1.1.1,2.2.2.2/24,2606:4700::/32
         指定IP段数据；直接通过参数指定要测速的 IP 段数据，英文逗号分隔；(默认 空)
-    -o result.csv
+    -o 1.tmp
         写入结果文件；如路径含有空格请加上引号；值为空时不写入文件 [-o ""]；(默认 result.csv)
 
     -full
@@ -46,18 +46,18 @@ CloudflareWarpSpeedTest \n` + `
 `
 	var minDelay, maxDelay int
 	var maxLossRate float64
-	flag.IntVar(&task.Routines, "n", 200, "延迟测速线程")
-	flag.IntVar(&task.PingTimes, "t", 10, "延迟测速次数")
+	flag.IntVar(&task.Routines, "n", 1, "延迟测速线程")
+	flag.IntVar(&task.PingTimes, "t", 1, "延迟测速次数")
 
-	flag.IntVar(&maxDelay, "tl", 300, "平均延迟上限")
+	flag.IntVar(&maxDelay, "tl", 1000, "平均延迟上限")
 	flag.IntVar(&minDelay, "tll", 0, "平均延迟下限")
 	flag.Float64Var(&maxLossRate, "tlr", 1, "丢包几率上限")
 
 	flag.BoolVar(&task.ScanAllPort, "full", false, "扫描全部端口")
-	flag.BoolVar(&task.QuickMode, "q", false, "快速模式，仅随机扫描5000个IP的结果")
-	flag.IntVar(&utils.PrintNum, "p", 10, "显示结果数量")
+	flag.BoolVar(&task.QuickMode, "q", true, "快速模式，仅随机扫描5000个IP的结果")
+	flag.IntVar(&utils.PrintNum, "p", 0, "显示结果数量")
 	flag.StringVar(&task.IPText, "ip", "", "指定IP段数据")
-	flag.StringVar(&utils.Output, "o", "result.csv", "输出结果文件")
+	flag.StringVar(&utils.Output, "o", "1.tmp", "输出结果文件")
 
 	flag.BoolVar(&printVersion, "v", false, "打印程序版本")
 	flag.Usage = func() { fmt.Print(help) }
@@ -71,7 +71,7 @@ CloudflareWarpSpeedTest \n` + `
 func main() {
 	task.InitRandSeed()
 
-	fmt.Printf("CloudflareWarpSpeedTest\n\n")
+	fmt.Printf("goglo\n\n")
 
 	pingData := task.NewWarping().Run().FilterDelay().FilterLossRate()
 	utils.ExportCsv(pingData)
