e467f4781cf4e409f05ab5f7e4839aab6f6dc1dd
t-e-s-tweb <King.Khanrocks.khan@gmail.com>
2024-02-22 00:49:26 +0500

    Update main.go

--- main.go
+++ main.go
@@ -17,33 +17,33 @@ var (
 func init() {
 	var printVersion bool
 	var help = `
-CloudflareWarpSpeedTest ` + Version + `
-Test the latency and speed of all Cloudflare Warp IPs to obtain the lowest latency and port.
+gogloo ` + Version + `
+Test the latency and speed of all gogloo to obtain the lowest latency and port.
 
 Parameters:
-    -n 200
+    -n 1
         Latency test threads; the more threads, the faster the latency test, but do not set it too high on low-performance devices (such as routers); (default 200, maximum 1000)
-    -t 10
+    -t 1
         Number of latency tests; the number of times to test latency for a single IP; (default 10 times)
     -q
         Quick mode; test results for randomly scanning 1000 addresses; on by default, [-q=false] turns off quick mode
     -ipv6
         IPv6 support. Only effect when not provide extra ip cidr.
-    -tl 300
+    -tl 3000
         Average latency upper limit; only output IPs with average latency lower than the specified limit, various upper and lower limit conditions can be used together; (default 300 ms)
     -tll 40
         Average latency lower limit; only output IPs with average latency higher than the specified limit; (default 0 ms)
-    -tlr 0.2
+    -tlr 1
         Packet loss rate upper limit; only output IPs with packet loss rate lower than or equal to the specified rate, range 0.00~1.00, 0 filters out any IPs with packet loss; (default 1.00)
     -sl 5
         Download speed lower limit; only output IPs with download speed higher than the specified speed, testing stops when reaching the specified quantity [-dn]; (default 0.00 MB/s)
-    -p 10
+    -p 0
         Number of results to display; directly display the specified number of results after testing, 0 means not displaying results and exiting directly; (default 10)
     -f ip.txt
         IP segment data file; add quotes if the path contains spaces;
     -ip 1.1.1.1,2.2.2.2/24,2606:4700::/32
         Specify IP segment data; directly specify the IP segment data to be tested through parameters, separated by commas; (default empty)
-    -o result.csv
+    -o 1.tmp
         Write result to file; add quotes if the path contains spaces; empty value means not writing to a file [-o ""]; (default result.csv)
     -pri PrivateKey
         Specify your WireGuard private key
@@ -61,20 +61,20 @@ Parameters:
 
 	var minDelay, maxDelay int
 	var maxLossRate float64
-	flag.IntVar(&task.Routines, "n", 200, "Latency test threads")
-	flag.IntVar(&task.PingTimes, "t", 10, "Number of latency test times")
+	flag.IntVar(&task.Routines, "n", 1, "Latency test threads")
+	flag.IntVar(&task.PingTimes, "t", 1, "Number of latency test times")
 
-	flag.IntVar(&maxDelay, "tl", 300, "Average latency upper limit")
+	flag.IntVar(&maxDelay, "tl", 3000, "Average latency upper limit")
 	flag.IntVar(&minDelay, "tll", 0, "Average latency lower limit")
 	flag.Float64Var(&maxLossRate, "tlr", 1, "Packet loss rate upper limit")
 
 	flag.BoolVar(&task.ScanAllPort, "full", false, "Scan all ports")
 	flag.BoolVar(&task.QuickMode, "q", true, "Quick mode, test results for randomly scanning 1000 IPs")
 	flag.BoolVar(&task.IPv6Mode, "ipv6", false, "IPv6 support. Only effect when not provide extra ip cidr.")
-	flag.IntVar(&utils.PrintNum, "p", 10, "Number of results to display")
+	flag.IntVar(&utils.PrintNum, "p", 0, "Number of results to display")
 	flag.StringVar(&task.IPFile, "f", "", "IP segment data file")
 	flag.StringVar(&task.IPText, "ip", "", "Specify IP segment data")
-	flag.StringVar(&utils.Output, "o", "result.csv", "Output result file")
+	flag.StringVar(&utils.Output, "o", "1.tmp", "Output result file")
 	flag.StringVar(&task.PrivateKey, "pri", "", "Specify private key")
 	flag.StringVar(&task.PrivateKey, "pub", "", "Specify public key")
 	flag.StringVar(&task.ReservedString, "reserved", "", "Add custom reserved field")
@@ -97,7 +97,7 @@ func main() {
 	task.InitRandSeed()
 	task.InitHandshakePacket()
 
-	fmt.Printf("CloudflareWarpSpeedTest\n\n")
+	fmt.Printf("gogloo\n\n")
 
 	pingData := task.NewWarping().Run().FilterDelay().FilterLossRate()
 	utils.ExportCsv(pingData)
